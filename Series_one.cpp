34fffd8f082b4a7147cca2d4f85bbbe728d18289
Artyom Rashevskiy <albatroz3000@yandex.ru>
2024-03-06 18:49:05 +0300

    Added bellNumber function

---
+++ Series_one.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+int main() {
+    long double sum, part_sum, k;
+    cin >> k;
+    sum = 0;
+    long long fac = 1;
+    for (long long n = 1; n < 50; n++) {
+        fac = fac * n;
+        part_sum = pow(n, k) / fac;
+        sum += part_sum;
+    }
+    cout << sum << endl;
+}
